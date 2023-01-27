b6c3f33b9776c69ffc74e6066215ba67cd3cdedd
Hee Seo Kim <hisounni@ewhain.net>
2023-01-27 14:45:41 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+    int a = 0, b = 0;
+    int sum = 0;
+    
+    scanf("%d %d", &a, &b);
+
+    sum = a + b;
+    printf("%d", sum);
+
+    return 0;
+}
