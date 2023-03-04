29dbfcfb118d09065a2e74b87cde4cefeefb387d
Hee Seo Kim <hisounni@ewhain.net>
2023-03-04 13:05:06 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+
+int main(void) {
+
+	int A, B;
+
+	cin >> A >> B;
+	cout << A + B << endl;
+	cout << A - B << endl;
+	cout << A * B << endl;
+	cout << A / B << endl;
+	cout << A % B;
+
+	return 0;
+}
